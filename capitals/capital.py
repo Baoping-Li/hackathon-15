b8a48c637953c9a856c265d6c47bb6794ff7d956
WMaja <wmaja@slb.com>
2016-12-08 14:43:38 -0600

    Removing 20 limit

--- capitals/capital.py
+++ capitals/capital.py
@@ -62,11 +62,7 @@ class Capital:
 
     def get_query_results(self, query):
         results = list()
-        i = 0;
         for entity in list(query.fetch()):
-            i += 1
-            if i > 20:
-               return results
             results.append(self.to_dto(entity))
         return results
 
