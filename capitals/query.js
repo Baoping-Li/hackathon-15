e95e74088a11e78f2d964390659698fd9ff2031c
WMaja <wmaja@slb.com>
2016-12-08 21:24:59 -0600

    Adding Google Maps in Polymer

--- capitals/query.js
+++ capitals/query.js
@@ -25,7 +25,7 @@ angular
 						if (distinct.indexOf(array[i].country) == -1)
 						{
 							distinct.push(array[i].country)
-							array[i].mapurl = "https://maps.google.com/maps?q=" + array[i].location.latitude + "," + array[i].location.longitude + "&hl=es;z=14&output=embed"
+							array[i].mapurl = "https://maps.google.com/maps?q=" + array[i].location.latitude + "," + array[i].location.longitude + "&hl=es&z=4&output=embed"
 							distinctCountries.push(array[i])
 						}
 						
@@ -79,3 +79,47 @@ angular
 			$scope.updateView();
 		});
 
+angular
+.module('mapsApp', [])
+.controller(
+		'mapsController',
+		function($scope, $sce, $http) {
+			var search = this;
+			$scope.capitals = []
+			$scope.updateCapitals = function() {
+				var $url = "https://capital-service-dot-hackathon-team-015.appspot.com/api/capitals/all"
+				//var $url = "http://localhost:8080/api/capitals/all"
+				$http.get($url).then(function(response) {
+					var distinct = []
+					var array = response.data
+					var distinctCountries = [] 
+					for (var i = 0; i < array.length; i++)
+						if (distinct.indexOf(array[i].country) == -1)
+						{
+							distinct.push(array[i].country)
+							array[i].mapurl = "https://maps.google.com/maps?q=" + array[i].location.latitude + "," + array[i].location.longitude + "&hl=es&z=4&output=embed"
+							distinctCountries.push(array[i])
+							var marker = document.createElement("google-map-marker");
+							var latitude = document.createAttribute("latitude");
+							latitude.value = array[i].location.latitude;
+							marker.setAttributeNode(latitude);
+							var longitude = document.createAttribute("longitude");
+							longitude.value = array[i].location.longitude;
+							marker.setAttributeNode(longitude);
+							var title = document.createAttribute("title");
+							title.value = array[i].name;
+							marker.setAttributeNode(title);
+							Polymer.dom(document.getElementById("googleMaps")).appendChild(marker);
+						}
+						
+						distinctCountries.sort(function(a, b){
+							if(a.country < b.country) return -1;
+							if(a.country > b.country) return 1;
+							return 0;
+						});
+
+						$scope.capitals = distinctCountries
+				});
+			}
+			$scope.updateCapitals();
+		});
